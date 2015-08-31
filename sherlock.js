9e991c3932d179a497508bc857b18a66a5ac7726
aldomunguiasu <aldomunguiasu@udlap.mx>
2015-08-31 17:04:12 -0500

    Tarea 2

--- sherlock.js
+++ sherlock.js
@@ -1,6 +1,6 @@
 //Input Example
 
-_INPUT = "2\n3 9\n17 24";
+//_INPUT = "2\n3 9\n17 25";
 
 //Expected Output
 /*
@@ -20,5 +20,33 @@ _INPUT = "2\n3 9\n17 24";
 0
 1
 */
+_INPUT = "6\n3 10\n1 25\n0 16\n1 8\n2 50\n1 8";  //prueba random
 
+//expected Output
+/*
+2
+
+*/
 //----- Start your code here -------
+div = _INPUT.split('\n');
+for(i=1;i<div.length;i++) //for contando pares de grupos
+{
+	valor = div[i].split(" "); //separa el par de numeros en otro array
+	cont_sqrt(parseInt(valor[0]), parseInt(valor[1]));
+
+}
+
+function cont_sqrt(a, b){
+	res_positivos=0;  //reinicia los numeros que son cuadrados perfectos
+	for(a;a<=b;a++)
+	{
+		raiz=Math.sqrt(a); //saca la raiz del numero en turno
+		if(raiz % 1 == 0) //si el residuo del numero es 0 es cuadrado perfecto
+		{  
+			res_positivos++;
+		}	
+	}
+	console.log(res_positivos); //imprime
+}
+
+
